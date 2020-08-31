fc7cbf0dd67d807fd972bdcc19c29f262035574d
Jed Guintu <41206975+j3ddy-l3n-f-gu1ntu@users.noreply.github.com>
2020-08-31 00:26:44 -0700

    did first case
    just need to account for when the warehouse items don't add up to the order

--- inventory-allocator/src/InventoryAllocator.js
+++ inventory-allocator/src/InventoryAllocator.js
@@ -23,8 +23,9 @@ class InventoryAllocator {
         for (const [key, value] of Object.entries(this.orders)) {
             let val = value;
             for(let i = 0; i < this.inv.length; i++){
+                // 1st case: we find a warehouse that has the exact number of whatever we're buying, all we have to do is push that info to our receipt varible
                 if(this.inv[i]["inventory"].hasOwnProperty(key) === true && this.inv[i]["inventory"][key] === value && val !== 0)  {
-                    val -= 0;
+                    val = 0;
                     let obj = {};
                     let info = {};
                     info[key] = value;
@@ -32,11 +33,29 @@ class InventoryAllocator {
                     receipt.push(obj);
                     //console.log(`${key} ${value}`)
                 }
+                // 2nd case: multiple warehouses needed to ship an item ex) apple:10, warehouse 1 has 3 apples, warehouse 2 has 7 apples
+                else if(this.inv[i]["inventory"].hasOwnProperty(key) === true && this.inv[i]["inventory"][key] < value && val !== 0) {
+                    val -= this.inv[i]["inventory"][key];
+                    let warehouseExist = false;
+                    let obj = {};
+                    let info = {};
+                    info[key] = this.inv[i]["inventory"][key];
+                    obj[this.inv[i]["name"]] = info;
+                    for(let j = 0; j < receipt.length; j++) {
+                        //console.log(receipt[j].hasOwnProperty(this.inv[i].name))
+                        if(receipt[j].hasOwnProperty(this.inv[i].name) === true) {
+                            warehouseExist = true;
+                            receipt[j][this.inv[i].name][key] = this.inv[i]["inventory"][key];
+                            break;
+                        }
+                    }
+                    (!warehouseExist ? receipt.push(obj) : console.log("warehouse true"))
+                }
             }
         }
 
-        console.log(this.orders);
-        console.log(this.inv);
+        //console.log(this.orders);
+        //console.log(this.inv);
         console.log(receipt)
     }
 
@@ -46,5 +65,5 @@ class InventoryAllocator {
 
 
 
-var ship = new InventoryAllocator({ apple: 1 }, [{ name: "owd", inventory: { apple: 1 } }]);
+var ship = new InventoryAllocator({ apple: 7 }, [{ name: `owd`, inventory: { apple: 2 } }, { name: `dm`, inventory: { apple: 5 }}]);
 ship.ship();
